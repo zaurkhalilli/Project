2d4ea842385e5c8a7d9508c0678ff77eaa972c1c
zaurkhalilli <ggencetel274@gmail.com>
2023-08-05 00:27:30 +0400

    new adds

--- project-demo/src/js/app.js
+++ project-demo/src/js/app.js
@@ -1,9 +1,9 @@
-// Get references to the button and dropdown content
-const dropdownButton = document.getElementById("dropdownButton");
-const dropdownContent = document.getElementById("dropdownContent");
+// Get references to the button and menu
+const menuButton = document.getElementById("menuButton");
+const menu = document.getElementById("menu");
 
 // Add a click event listener to the button
-dropdownButton.addEventListener("click", () => {
-  // Toggle the display of the dropdown content
-  dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
+menuButton.addEventListener("click", () => {
+  // Toggle the 'hidden' class on the menu to show/hide it
+  menu.classList.toggle("hidden");
 });
