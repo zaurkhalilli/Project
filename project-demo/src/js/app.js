e14028693861c6f90d6fdac1a6ac68e38bd4e78b
zaurkhalilli <ggencetel274@gmail.com>
2023-08-05 23:55:43 +0400

    swiper added

--- project-demo/src/js/app.js
+++ project-demo/src/js/app.js
@@ -1,9 +1,51 @@
-// Get references to the button and menu
-const menuButton = document.getElementById("menuButton");
-const menu = document.getElementById("menu");
 
-// Add a click event listener to the button
-menuButton.addEventListener("click", () => {
-  // Toggle the 'hidden' class on the menu to show/hide it
-  menu.classList.toggle("hidden");
+var swiper = new Swiper(".mySwiper-", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+var swiper = new Swiper(".mySwiperr", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".myswiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
 });
